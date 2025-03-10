522511891a01572fce2bfb4e2018679d80a37f46
Vishwateja Domakonda <vishwatejadomakonda@gmail.com>
2025-03-10 16:11:49 +0530

    UI changes

--- app.py
+++ app.py
@@ -14,23 +14,28 @@ db = SQLAlchemy(app)
 
 @app.route('/')
 def dashboard():
-    return render_template("index.html")
+    return render_template("index.html", active_page="dashboard")
+
 
 @app.route("/login/")
 def login():
-    return render_template("login.html")
+    return render_template("login.html", active_page="login")
+
 
 @app.route("/registration/")
 def registration():
     return render_template("registration.html")
 
+
 @app.route("/add-recipe/")
 def add_recipe():
-    return render_template("add_recipe.html")
+    return render_template("add_recipe.html", active_page="add_recipe")
+
 
 @app.route("/profile/")
 def profile():
-    return render_template("my_profile.html")
+    return render_template("my_profile.html", active_page="profile")
+
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0")
